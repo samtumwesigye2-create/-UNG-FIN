70395d421872d6dc67584ab7e26440598301d9a1
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-12 02:06:08 -0400

    feat: expose MIDAS treasury and reporting APIs

--- entrypoint.py
+++ entrypoint.py
@@ -2,6 +2,10 @@ from app import app
 from finance_kpis import router as finance_kpis_router
 from accounting import router as accounting_router
 from ap import router as ap_router
+from treasury import router as treasury_router
+from reporting import router as reporting_router
 app.include_router(finance_kpis_router)
 app.include_router(accounting_router)
 app.include_router(ap_router)
+app.include_router(treasury_router)
+app.include_router(reporting_router)
