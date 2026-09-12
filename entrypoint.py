11a59c4f66bdbe4fa925ac471226bbb21d7d5a79
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-12 02:08:24 -0400

    fix: avoid duplicate router mounting in entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,1 @@
 from app import app
-from finance_kpis import router as finance_kpis_router
-from accounting import router as accounting_router
-from ap import router as ap_router
-app.include_router(finance_kpis_router)
-app.include_router(accounting_router)
-app.include_router(ap_router)
